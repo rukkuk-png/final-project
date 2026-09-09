8431c286137122e243a7f10ab607ad843bbe5901
rukkuk <rukku.k@gmail.com>
2026-09-09 00:40:10 -0400

    final changes

--- server.py
+++ server.py
@@ -4,31 +4,34 @@
 '''
 # Import Flask, render_template, request from the flask pramework package :
 from flask import Flask, render_template, request
-# Import the sentiment_analyzer function from the package created:
+# Import the emotion_detector function from the package created:
 from EmotionDetection.emotion_detection import emotion_detector
 
 #Initiate the flask app :
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def sent_analyzer():
+def emo_detector():
     ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using emotion_detector()
+        runs Emotion detection over it using emotion_detector()
         function. The output returned shows the details for the provided text.
     '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
-    # Pass the text to the sentiment_analyzer function and store the response
+    # Pass the text to the emotion detection function and store the response
     response = emotion_detector(text_to_analyze)
-    # Extract the label and score from the response
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
+    # Extract the response
+    an = response['anger']
+    di = response['disgust']
+    fe = response['fear']
     joy =  response['joy']
-    sadness = response['sadness']
-    dominant_emotion = response['dominant_emotion']
-    # Return a formatted string with the emotional detector
-    return "For the given statement, the system response is 'anger':{}, 'disgust':{}, 'fear':{}, 'joy':{}and 'sadness':{}. The dominant emotion is {}.".format(anger,disgust,fear,joy,sadness,dominant_emotion)
+    sn = response['sadness']
+    de = response['dominant_emotion']
+    if de is None:
+        return "Invalid text! Please try again!."
+
+    #return f"'anger':{an},'disgust':{di},'fear':{fe},'joy':{joy},'sadness':{sn}.Result{de}"
+    return "For the given statement, the system response is 'anger':{}, 'disgust':{}, 'fear':{}, 'joy':{} and 'sadness':{}. The dominant emotion is {}.".format(anger,disgust,fear,joy,sadness,dominant_emotion)
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
